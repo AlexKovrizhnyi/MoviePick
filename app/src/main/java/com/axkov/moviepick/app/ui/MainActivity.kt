422d08158df172c842637a5826b9e21d990a7535
Alex Kovrizhnyi <kovrizhnyi@gmail.com>
2021-09-10 17:43:24 +0300

    Add navigation

---
+++ app/src/main/java/com/axkov/moviepick/app/ui/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.axkov.moviepick.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.axkov.moviepick.app.R
+
+class MainActivity : AppCompatActivity() {
+    private var _navController: NavController? = null
+    private val navController get() = checkNotNull(_navController) { "NavController isn't initialized" }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.activity_main_nav_host_fragment) as NavHost
+
+        _navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
