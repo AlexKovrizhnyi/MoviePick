5f9069062995500e9fac5fe5d36f1c81c0db7628
Alex Kovrizhnyi <kovrizhnyi@gmail.com>
2021-09-10 16:52:38 +0300

    Add BaseViewModel and BaseFragment

---
+++ app/src/main/java/com/axkov/moviepick/app/ui/base/BaseFragment.kt
@@ -0,0 +1,20 @@
+package com.axkov.moviepick.app.ui.base
+
+import android.view.View
+import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+abstract class BaseFragment: Fragment() {
+    protected lateinit var progressBar: ProgressBar
+
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private fun showProgressBar(isLoading: Boolean) {
+        if (isLoading) {
+            progressBar.visibility = View.VISIBLE
+        } else {
+            progressBar.visibility = View.GONE
+        }
+    }
+}
---
+++ app/src/main/java/com/axkov/moviepick/app/ui/base/BaseViewModel.kt
@@ -0,0 +1,22 @@
+package com.axkov.moviepick.app.ui.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+abstract class BaseViewModel: ViewModel() {
+    protected val compositeDisposable = CompositeDisposable()
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean>
+        get() = _isLoading
+
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String>
+        get() = _errorMessage
+
+    override fun onCleared() {
+        compositeDisposable.dispose()
+    }
+}
