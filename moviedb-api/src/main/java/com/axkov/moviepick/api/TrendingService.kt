8bf7f1c16d110fceeb3090b2be57002284bab7e4
Alex Kovrizhnyi <kovrizhnyi@gmail.com>
2021-09-29 22:10:23 +0300

    Change the moviedb-api module to simple java module

--- moviedb-api/src/main/java/com/axkov/moviepick/api/TrendingService.kt
+++ moviedb-api/src/main/java/com/axkov/moviepick/api/TrendingService.kt
@@ -2,11 +2,6 @@ package com.axkov.moviepick.api
 
 import com.axkov.moviepick.api.entities.TrendingResponse
 import io.reactivex.rxjava3.core.Single
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -20,25 +15,4 @@ interface TrendingService {
     fun getTrendingWeek(
         @Query("api_key") apiKey: String
     ): Single<TrendingResponse>
-}
-
-fun TrendingService(): TrendingService {
-    val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor().apply {
-            level = if (BuildConfig.DEBUG) {
-                HttpLoggingInterceptor.Level.BODY
-            } else {
-                HttpLoggingInterceptor.Level.NONE
-            }
-        })
-        .build()
-
-    val retrofit = Retrofit.Builder()
-        .baseUrl(TrendingService.BASE_URL)
-        .client(okHttpClient)
-        .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-        .build()
-
-    return retrofit.create(TrendingService::class.java)
 }
