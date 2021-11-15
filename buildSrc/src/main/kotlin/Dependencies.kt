b287d35d3dd5d926472faa8cf7cd35a744e12855
Alex Kovrizhnyi <kovrizhnyi@gmail.com>
2021-11-15 19:36:37 +0200

    Change gradle version to 7.0.3

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -17,7 +17,7 @@ object AndroidClient {
 
 object BuildPlugins {
     object Versions {
-        const val gradleVersion = "7.0.2"
+        const val gradleVersion = "7.0.3"
         const val navigation = "2.3.5"
     }
 
