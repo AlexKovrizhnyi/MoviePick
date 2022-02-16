9a40a19a01e932aa1289ec7fd12ba35064fbf480
Alex Kovrizhnyi <kovrizhnyi@gmail.com>
2022-02-16 15:30:35 +0200

    Change Movie domain model

---
+++ domain/src/main/java/com/axkov/moviepick/domain/models/Category.kt
@@ -0,0 +1,8 @@
+package com.axkov.moviepick.domain.models
+
+object Category {
+    const val TRENDING = "trending"
+    const val POPULAR = "popular"
+    const val TOP_RATED = "top_rated"
+    const val UPCOMING = "upcoming"
+}
--- domain/src/main/java/com/axkov/moviepick/domain/models/Movie.kt
+++ domain/src/main/java/com/axkov/moviepick/domain/models/Movie.kt
@@ -2,6 +2,20 @@ package com.axkov.moviepick.domain.models
 
 data class Movie(
     val id: Long,
+
     val title: String,
-    val poster: String
+
+    val originalTitle: String,
+
+    val overview: String,
+
+    val posterPath: String,
+
+    val backdropPath: String,
+
+//    val runtime: Int,
+
+    val voteAverage: Double,
+
+    val voteCount: Int
 )
